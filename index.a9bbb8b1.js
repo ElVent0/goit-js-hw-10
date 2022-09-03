ef9e2b2065175b7814b2e61c21723705bbf04419
ElVent0 <91426858+ElVent0@users.noreply.github.com>
2022-09-03 09:34:08 +0000

    Deploying to gh-pages from @ ElVent0/goit-js-hw-10@7b501a2c5140f0175e4d8c366a3cd929451aa635 🚀

--- index.a9bbb8b1.js
+++
@@ -1,2 +0,0 @@
-function e(e){return fetch(`https://restcountries.com/v3.1/name/${e}`).then((e=>e.json()))}document.querySelector("#search-box").addEventListener("input",(function(n){e(n.currentTarget.value).then((e=>{e.map((e=>{e.name})).join("")}))}));
-//# sourceMappingURL=index.a9bbb8b1.js.map
